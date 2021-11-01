ca779d8ba8b4f556e697cb6d0d456dc7ccd8f2eb
Kurt <kurtmtthw@gmail.com>
2021-11-01 21:14:05 +0800

    Updated comments.

--- src/midlab2/Utility.java
+++ src/midlab2/Utility.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public class Utility {
     // TODO: 10/30/2021 @Enrico 
+    /*
+        Concatenates the inputted text into a single
+        StringBuilder variable
+     */
     public StringBuilder acceptInput() {
         return null;
     }
 
-    // TODO: 10/30/2021 @CJ
-    // Use downcasting when creating a List instance
-    // List<Token<T>> list = new ArrayList<>();
+    /* TODO: 10/30/2021 @CJ
+       Determines the frequency of each character
+       and sets the appropriate value for
+       frequency in the Token class
+       Use downcasting when creating a List instance:
+       List<Token<T>> list = new ArrayList<>();
+     */
+
     public <T> List<Token<T>> determineFrequency(StringBuilder userText) {
         return null;
     }
@@ -20,14 +29,17 @@ public class Utility {
         return null;
     }
 
-    // TODO: 10/30/2021 @Jerome 
+    /* TODO: 10/30/2021 @Jerome
+       Sets up the Forest of Trees
+       Similar to that of page 22
+     */
     public <T> Tree<T> forestBuilder() {
         return null;
     }
 
-    // TODO: 10/30/2021 @Kurt 
+    // TODO: 10/30/2021 @Kurt
+    // To be used with forestBuilder
     public <T> void setHuffmanCode(Tree<T> forest) {
-
     }
 
     public <T> void showHuffmanTable(List<Token<T>> tokenList) {
